6f4ae788778474f47b56d8b365533df5f2a3300a
Alexander Yustus <alexander.yustus@xored.com>
2020-02-25 18:57:10 +0700

    wip
    Signed-off-by: Alexander Yustus <alexander.yustus@xored.com>

---
+++ cmd/nsmd/main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+
+	"github.com/networkservicemesh/networkservicemesh/controlplane/pkg/model"
+	"github.com/networkservicemesh/networkservicemesh/controlplane/pkg/nsm"
+	"github.com/networkservicemesh/networkservicemesh/controlplane/pkg/nsmd"
+	"github.com/networkservicemesh/networkservicemesh/pkg/probes"
+	"github.com/networkservicemesh/cmd-nsmd/pkg/tools"
+	"github.com/networkservicemesh/sdk/pkg/tools/jaeger"
+	"github.com/networkservicemesh/sdk/pkg/tools/spanhelper"
+	"github.com/networkservicemesh/networkservicemesh/utils"
+)
+
+var version string
+
+// Default values and environment variables of proxy connection
+const (
+	NsmdAPIAddressEnv      = "NSMD_API_ADDRESS"
+	NsmdAPIAddressDefaults = ":5001"
+)
+
+func main() {
+	logrus.Info("Starting nsmd...")
+	logrus.Infof("Version: %v", version)
+	utils.PrintAllEnv(logrus.StandardLogger())
+	start := time.Now()
+
+	// Capture signals to cleanup before exiting
+	c := tools.NewOSSignalChannel()
+
+	closer := jaeger.InitJaeger("nsmd")
+	defer func() { _ = closer.Close() }()
+
+	// Global NSMgr server span holder
+	span := spanhelper.FromContext(context.Background(), "nsmd.server")
+	span.LogValue("tracing.init-complete", fmt.Sprintf("%v", time.Since(start)))
+	defer span.Finish() // Mark it as finished, since it will be used as root.
+
+	apiRegistry := nsmd.NewApiRegistry()
+	serviceRegistry := nsmd.NewServiceRegistry()
+
+	model := model.NewModel() // This is TCP gRPC server uri to access this NSMD via network.
+	defer serviceRegistry.Stop()
+	manager := nsm.NewNetworkServiceManager(span.Context(), model, serviceRegistry)
+
+	var server nsmd.NSMServer
+	var srvErr error
+	// Start NSMD server first, load local NSE/client registry and only then start forwarder/wait for it and recover active connections.
+
+	if server, srvErr = nsmd.StartNSMServer(span.Context(), model, manager, apiRegistry); srvErr != nil {
+		logrus.Errorf("error starting nsmd service: %+v", srvErr)
+		return
+	}
+	defer server.Stop()
+
+	nsmdGoals := &nsmdProbeGoals{}
+	nsmdProbes := probes.New("NSMD liveness/readiness healthcheck", nsmdGoals)
+	nsmdProbes.BeginHealthCheck()
+
+	logrus.Info("NSM server is ready")
+	nsmdGoals.SetNsmServerReady()
+
+	// Register CrossConnect monitorCrossConnectServer client as ModelListener
+	monitorCrossConnectClient := nsmd.NewMonitorCrossConnectClient(model, server, server.XconManager(), server)
+	model.AddListener(monitorCrossConnectClient)
+
+	// Starting forwarder
+	logrus.Info("Starting Forwarder registration server...")
+	if err := server.StartForwarderRegistratorServer(span.Context()); err != nil {
+		span.LogError(errors.Wrap(err, "error starting forwarder service"))
+		return
+	}
+
+	// Wait for forwarder to be connecting to us
+	if err := manager.WaitForForwarder(span.Context(), nsmd.ForwarderTimeout); err != nil {
+		span.LogError(errors.Wrap(err, "error waiting for forwarder"))
+		return
+	}
+
+	span.Logger().Info("Forwarder server is ready")
+	nsmdGoals.SetForwarderServerReady()
+	// Choose a public API listener
+
+	nsmdAPIAddress := getNsmdAPIAddress()
+	span.LogObject("api-address", nsmdAPIAddress)
+	sock, err := apiRegistry.NewPublicListener(nsmdAPIAddress)
+	if err != nil {
+		span.LogError(errors.Wrap(err, "failed to start Public API server: %+v"))
+		return
+	}
+	span.Logger().Info("Public listener is ready")
+	nsmdGoals.SetPublicListenerReady()
+
+	server.StartAPIServerAt(span.Context(), sock, nsmdProbes)
+	nsmdGoals.SetServerAPIReady()
+	span.Logger().Info("Serve api is ready")
+
+	span.LogValue("start-time", fmt.Sprintf("%v", time.Since(start)))
+	span.Finish()
+	<-c
+}
+
+func getNsmdAPIAddress() string {
+	result := os.Getenv(NsmdAPIAddressEnv)
+	if strings.TrimSpace(result) == "" {
+		result = NsmdAPIAddressDefaults
+	}
+	return result
+}
---
+++ cmd/proxy-nsmd/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"context"
+	"net"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/networkservicemesh/networkservicemesh/utils"
+
+	"github.com/networkservicemesh/networkservicemesh/pkg/probes/health"
+	"github.com/networkservicemesh/networkservicemesh/pkg/tools/jaeger"
+
+	"github.com/networkservicemesh/networkservicemesh/pkg/probes"
+
+	"github.com/networkservicemesh/networkservicemesh/controlplane/pkg/nsmd"
+	"github.com/networkservicemesh/networkservicemesh/controlplane/pkg/serviceregistry"
+	"github.com/networkservicemesh/networkservicemesh/sdk/monitor/remote"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/networkservicemesh/networkservicemesh/controlplane/api/connection"
+	unified "github.com/networkservicemesh/networkservicemesh/controlplane/api/networkservice"
+	proxynetworkserviceserver "github.com/networkservicemesh/networkservicemesh/controlplane/pkg/remote/proxy_network_service_server"
+	"github.com/networkservicemesh/networkservicemesh/pkg/tools"
+)
+
+var version string
+
+// Default values and environment variables of proxy connection
+const (
+	ProxyNsmdAPIAddressEnv      = "PROXY_NSMD_API_ADDRESS"
+	ProxyNsmdAPIAddressDefaults = ":5006"
+)
+
+func main() {
+	logrus.Info("Starting proxy nsmd...")
+	logrus.Infof("Version: %v", version)
+	utils.PrintAllEnv(logrus.StandardLogger())
+	start := time.Now()
+
+	// Capture signals to cleanup before exiting
+	c := tools.NewOSSignalChannel()
+
+	closer := jaeger.InitJaeger("proxy-nsmd")
+	defer func() { _ = closer.Close() }()
+	goals := &proxyNsmdProbeGoals{}
+	nsmdProbes := probes.New("Prxoy NSMD liveness/readiness healthcheck", goals)
+	nsmdProbes.BeginHealthCheck()
+
+	apiRegistry := nsmd.NewApiRegistry()
+	serviceRegistry := nsmd.NewServiceRegistry()
+	defer serviceRegistry.Stop()
+
+	// Choose a public API listener
+
+	sock, err := apiRegistry.NewPublicListener(getProxyNSMDAPIAddress())
+	if err != nil {
+		logrus.Errorf("Failed to start Public API server...")
+		return
+	}
+	logrus.Info("Public listener is ready")
+	goals.SetPublicListenerReady()
+
+	startAPIServerAt(sock, serviceRegistry, nsmdProbes)
+	logrus.Info("API server is ready")
+	goals.SetServerAPIReady()
+
+	elapsed := time.Since(start)
+	logrus.Debugf("Starting Proxy NSMD took: %s", elapsed)
+
+	<-c
+}
+
+func getProxyNSMDAPIAddress() string {
+	result := os.Getenv(ProxyNsmdAPIAddressEnv)
+	if strings.TrimSpace(result) == "" {
+		result = ProxyNsmdAPIAddressDefaults
+	}
+	return result
+}
+
+// StartAPIServerAt starts GRPC API server at sock
+func startAPIServerAt(sock net.Listener, serviceRegistry serviceregistry.ServiceRegistry, probes probes.Probes) {
+	grpcServer := tools.NewServer(context.Background())
+	remoteConnectionMonitor := remote.NewProxyMonitorServer()
+	connection.RegisterMonitorConnectionServer(grpcServer, remoteConnectionMonitor)
+	probes.Append(health.NewGrpcHealth(grpcServer, sock.Addr(), time.Minute))
+	// Register Remote NetworkServiceManager
+	remoteServer := proxynetworkserviceserver.NewProxyNetworkServiceServer(serviceRegistry)
+	unified.RegisterNetworkServiceServer(grpcServer, remoteServer)
+
+	go func() {
+		if err := grpcServer.Serve(sock); err != nil {
+			logrus.Fatalf("Failed to start NSM API server %+v", err)
+		}
+	}()
+	logrus.Infof("NSM gRPC API Server: %s is operational", sock.Addr().String())
+}
